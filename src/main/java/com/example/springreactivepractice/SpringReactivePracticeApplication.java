35194d1707943c41ac901befe728e031ddb20a2d
eunjune <iws1605@naver.com>
2022-04-10 15:19:47 +0900

    Initial commit

---
+++ src/main/java/com/example/springreactivepractice/SpringReactivePracticeApplication.java
@@ -0,0 +1,15 @@
+package com.example.springreactivepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Iterator;
+
+@SpringBootApplication
+public class SpringReactivePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactivePracticeApplication.class, args);
+    }
+
+}
