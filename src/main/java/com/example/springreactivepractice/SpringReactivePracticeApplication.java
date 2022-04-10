5a26ce41ea07bfb897df76b7dc275ebbd59cac31
eunjune <iws1605@naver.com>
2022-04-10 15:27:54 +0900

    Iterable

--- src/main/java/com/example/springreactivepractice/SpringReactivePracticeApplication.java
+++ src/main/java/com/example/springreactivepractice/SpringReactivePracticeApplication.java
@@ -4,11 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Iterator;
+import java.util.Observable;
 
 @SpringBootApplication
 public class SpringReactivePracticeApplication {
 
     public static void main(String[] args) {
+
+        Iterable<Integer> iter = () -> new Iterator<>() {
+            int i = 0;
+            final static int MAX = 10;
+
+            @Override
+            public boolean hasNext() {
+                return i < MAX;
+            }
+
+            @Override
+            public Integer next() {
+                return ++i;
+            }
+        };
+
+        for(Integer i : iter) {
+            System.out.println(i);
+        }
+
+        for (Iterator<Integer> it = iter.iterator(); it.hasNext();) {
+            System.out.println(it.next());
+        }
+
         SpringApplication.run(SpringReactivePracticeApplication.class, args);
     }
 
