034cea9c037b189e30f4eca68e35a9be331a36bf
eunjune <iws1605@naver.com>
2022-04-16 20:26:22 +0900

    onPublish 구현하기

--- src/main/java/com/example/springreactivepractice/SchedulerEx.java
+++ src/main/java/com/example/springreactivepractice/SchedulerEx.java
@@ -7,12 +7,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
 
-// subscribeOn : 데이터 처리과정들을 on에 지정한 스레드 안에서 수행을 해달라는 메소드
-// publishOn :
-
 @Slf4j
 public class SchedulerEx {
-    public static void main(String[] args) {
+    // onSubscribe : 데이터 처리과정들을 on에 지정한 스레드 안에서 수행을 해달라는 메소드
+    public void runOnSubscribe() {
         Flow.Publisher<Integer> pub = subscriber -> {
             subscriber.onSubscribe(new Flow.Subscription() {
                 @Override
@@ -47,6 +45,86 @@ public class SchedulerEx {
         };
 
         subOnPub.subscribe(new Flow.Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                log.debug("onSubscribe");
+                subscription.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                log.debug("onNext:{}",item);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                log.debug("onError:{}",throwable);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        });
+    }
+
+    // onPublish : 데이터 소비를 on에 지정한 스레드 안에서 수행을 해달라는 메소드
+    public void runOnPublish(String[] args) {
+        Flow.Publisher<Integer> pub = subscriber -> {
+            subscriber.onSubscribe(new Flow.Subscription() {
+                @Override
+                public void request(long n) {
+                    subscriber.onNext(1);
+                    subscriber.onNext(2);
+                    subscriber.onNext(3);
+                    subscriber.onNext(4);
+                    subscriber.onNext(5);
+                    subscriber.onComplete();
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+        };
+
+        Flow.Publisher<Integer> pubOnPub = sub -> {
+            pub.subscribe(new Flow.Subscriber<Integer>() {
+                /*
+                * 20:19:30.669 [main] DEBUG com.example.springreactivepractice.SchedulerEx - onSubscribe
+                * 20:19:30.672 [pool-1-thread-1] DEBUG com.example.springreactivepractice.SchedulerEx - onNext:1
+                * 20:19:30.673 [pool-1-thread-1] DEBUG com.example.springreactivepractice.SchedulerEx - onNext:2
+                * 20:19:30.674 [pool-1-thread-1] DEBUG com.example.springreactivepractice.SchedulerEx - onNext:3
+                * 20:19:30.674 [pool-1-thread-1] DEBUG com.example.springreactivepractice.SchedulerEx - onNext:4
+                * 20:19:30.674 [pool-1-thread-1] DEBUG com.example.springreactivepractice.SchedulerEx - onNext:5
+                * 20:19:30.674 [pool-1-thread-1] DEBUG com.example.springreactivepractice.SchedulerEx - onComplete
+                * */
+                ExecutorService es = Executors.newSingleThreadExecutor();
+
+                @Override
+                public void onSubscribe(Flow.Subscription subscription) {
+                    sub.onSubscribe(subscription);
+                }
+
+                @Override
+                public void onNext(Integer item) {
+                    es.execute(()->sub.onNext(item));
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    es.execute(()->sub.onError(throwable));
+                }
+
+                @Override
+                public void onComplete() {
+                    es.execute(()->sub.onComplete());
+                }
+            });
+        };
+
+        pubOnPub.subscribe(new Flow.Subscriber<Integer>() {
             @Override
             public void onSubscribe(Flow.Subscription subscription) {
                 log.debug("onSubscribe");
